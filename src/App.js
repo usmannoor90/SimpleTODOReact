7240853ed8c99b97257a57094aaf209ee81eaf58
Usman Noor <m.usmannoor90@gmail.com>
2023-06-23 16:46:55 +0500

    final code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./style.css";
 function App() {
+  const [newItem, setNewItem] = useState("");
+
+  const [todo, setTodo] = useState([]);
+
+  function handlerSubmit(e) {
+    e.preventDefault();
+
+    if (newItem.length === 0) {
+      return;
+    } else {
+      setTodo((creatItem) => {
+        return [
+          ...creatItem,
+          {
+            id: crypto.randomUUID(),
+            title: newItem,
+            completed: false,
+          },
+        ];
+      });
+    }
+
+    setNewItem("");
+  }
+
+  function todoChecked(Id, completed) {
+    setTodo((currentTodo) => {
+      return currentTodo.map((todo) => {
+        if (todo.id === Id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function handlerRemove(id) {
+    setTodo((currentTodo) => {
+      return currentTodo.filter((todo) => todo.id !== id);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: "400px" }}>
+      <form className="todoForm" onSubmit={handlerSubmit}>
+        <label>my Tod List</label>
+        <input
+          type="text"
+          className="todoInput"
+          value={newItem}
+          onChange={(e) => {
+            setNewItem(e.target.value);
+          }}
+        />
+        <button className="addBtn">Add</button>
+      </form>
+      <ul>
+        {todo.length === 0 && "no todos"}
+        {todo.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onClick={(e) => {
+                  todoChecked(todo.id, e.target.checked);
+                }}
+              />
+              <h1 className="itemName">{todo.title}</h1>
+              <button
+                className="deleteBtn"
+                onClick={() => {
+                  handlerRemove(todo.id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
